36cba18cea0f5763c6b742af024021876bba7c4b
Julia_DM <97304652+YuliyaDM@users.noreply.github.com>
2022-08-13 22:37:00 +0300

    Update withRouter.tsx

--- src/common/functions/withRouter.tsx
+++ src/common/functions/withRouter.tsx
@@ -3,16 +3,13 @@ import { useNavigate } from "react-router-dom";
 
 export default function withRouter(Component: any) {
     function ComponentWithRouterProp(props: any) {
-      const location = useLocation();
-      const navigate = useNavigate();
       const params = useParams();
       return (
         <Component
           {...props}
-          router={{ location, navigate, params }}
+          router={{ params }}
         />
       );
-    }
-  
+    } 
     return ComponentWithRouterProp;
 }
